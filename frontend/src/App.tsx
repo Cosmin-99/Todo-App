3400011148be1a07ad54d52ee9e40101b1ea28e9
unknown <susara.cosmin@gmail.com>
2020-08-19 21:38:43 +0300

    Small bugs fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { AddToDo, Todo } from "./AddTodoList";
 import { Menu } from './Menu';
 import { Show } from './showToDo';
-import moment from 'moment';
 import { Carousel } from './Carousel';
-import {Details} from './Details'
 
 
 function App() {
 
   const [data, setData] = useState<Todo[]>([])
-  
+
   useEffect(() => {
     document.title = "Todo App";
   })
@@ -34,14 +32,11 @@ function App() {
   //getData();
 
   return (
-    <Router>     
+    <Router>
       <Menu />
-      <Route path="/" >
-        <Carousel />
-        <Details />
-        </Route>
+      <Route path="/" exact component = {Carousel}/>
       <Route path="/TodoList" >
-         <Show  datas = {data} setData = {setData}/>
+        <Show datas={data} setData={setData} />
       </Route>
 
       <Route path="/AddToDo" >
--- frontend/src/Carousel.tsx
+++ frontend/src/Carousel.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import photo1 from "./photos/photo1.jpg";
 import photo2 from "./photos/photo2.jpg";
 import photo3 from "./photos/photo3.jpg";
+import { Details } from "./Details";
 
 export function Carousel() {
 
     return (
+        <>
         <div className = "carousel slide" data-ride = "carousel">
             <ul className = "carousel-indicators">
                 <li data-target = "#slides" data-slide-to  = "0" className = "active"></li>
@@ -25,5 +27,7 @@ export function Carousel() {
                 </div>
             </div>
         </div>
+    <Details />
+    </>
     );
 }
