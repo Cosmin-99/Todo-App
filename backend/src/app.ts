3f4798b29e1fc1c3394e4a397b5a30abcbea58d9
Cosmin <susara.cosmin@gmail.com>
2020-11-25 15:33:43 +0200

    Allow CORS policy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,5 +9,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(router);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.listen(5000, () => console.log('Server running on port', 5000))
